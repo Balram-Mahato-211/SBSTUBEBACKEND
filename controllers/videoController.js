3425f2bf70674cb68fa87452895a3cd8bbdb2346
Balram Mahato <balramegrks@gmail.com>
2026-08-10 07:54:58 +0530

    remove large temporary file

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -1,4 +1,4 @@
-const Video = require("../models/video");
+const Video = require("../models/Video");
 const User = require("../models/User");
 const jwt = require("jsonwebtoken");
 const cloudinary = require("cloudinary").v2;
